8fb4a3334dec14d8e555bb6e8ab615b6a2660cdb
gokintosh <gokul.kannambally_balachandran@student.po.edu.pl>
2022-09-12 18:20:41 +0200

    init proj

---
+++ src/main/java/com/gokul/kubecloud/KubeCloudApplication.java
@@ -0,0 +1,13 @@
+package com.gokul.kubecloud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KubeCloudApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(KubeCloudApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/gokul/kubecloud/KubeCloudApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gokul.kubecloud;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KubeCloudApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
